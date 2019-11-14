eb7ff85eb25e3f91e8cf883628f247fe97489860
Garrison Shepard <shepard.garrisont@gmail.com>
2019-11-14 11:13:37 -0500

    init commit

---
+++ AniChat/AniChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AniChat
+//
+//  Created by Garrison Shepard on 11/14/19.
+//  Copyright © 2019 Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
